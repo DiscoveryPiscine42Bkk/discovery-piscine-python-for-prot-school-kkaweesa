575e2859e51deb7c0dd4a8115f09c7810b81a8fa
kkaweesa <42900@prot.ac.th>
2025-03-13 13:25:59 +0700

    Add files via upload

---
+++ cell05/ex00/create_array.py
@@ -0,0 +1,5 @@
+namber1,namber2,namber3,namber4,namber5,namber6,namber7,namber8,namber9 = 2,4,8,45,00,0,-9,-23,-20
+print([namber1,namber2,namber3,namber4,namber5,namber6,namber7,namber8,namber9])
+
+array = [2, 8, 9, 48, 8, 22,-12, 2]
+print(array)
